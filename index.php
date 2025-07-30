91261df105726bda874da6900ae18c0a8d554191
Melvin1032 <jmragmamacabeo67@gmail.com>
2025-07-30 23:09:36 +0800

    design: add base layout for landing page

--- index.php
+++ index.php
@@ -3,11 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="./assets/img/Logo.png">
+    <link rel="icon" href="assets/img/Logo.png">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <title>Resource Management System</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Resource Management System </title>
 </head>
 <body>
+<div class="flex-container">
+  <div class="d1-maroon">
+    <div class="ispsc-logo">
+        <img src="assets/img/ISPSC-logo.png" alt="">
+    </div>
+  </div>
+
+  <div class="d2-yellow">
+    <div class="title">
+        <h2>ISPSC CANDON CAMPUS</h2>
+        <h1>SUPPLY OFFICE</h1>
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
